287ba2fce0884db0d1e32923540e19c175e3f991
Bruno Mzr <36006060+brunomzr@users.noreply.github.com>
2019-05-13 15:06:10 -0300

    Comment middleware
    Comment
    #'request_vars.middleware.RequestVarsMiddleware'

--- magalu/magalu/settings.py
+++ magalu/magalu/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'request_vars.middleware.RequestVarsMiddleware',
+    #'request_vars.middleware.RequestVarsMiddleware',
     'easyaudit.middleware.easyaudit.EasyAuditMiddleware',
 ]
 
